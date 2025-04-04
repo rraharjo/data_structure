d2c82e589055cf0adce130f54c7c72c540d6fb6d
rraharjo <raharjorobert2@gmail.com>
2025-04-03 23:17:25 -0400

    added deletion, tested

--- test_folder/BTree/btree.cpp
+++ test_folder/BTree/btree.cpp
@@ -3,10 +3,17 @@
 using namespace ds::btree;
 #define DEG 4
 int main(){
-    BTree<int> *tree = new BTree<int>(4);
-    for (int i = 1 ; i < 20 ; i++){
+    BTree<int> *tree = new BTree<int>(2);
+    for (int i = 1 ; i < 11 ; i++){
         tree->insert(i);
     }
+    tree->remove(6);
+    tree->remove(1);
+    tree->remove(3);
+    tree->remove(2);
+    tree->remove(2);
+    tree->remove(2);
+    tree->remove(8);
     std::cout << tree->to_string() << std::endl;
     delete tree;
 }
